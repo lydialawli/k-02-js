191623ffe556f5657ad0c276099c4c27f2a069f8
Ly <lydialawli@gmail.com>
2019-08-15 12:23:27 +0700

    added task 20103

--- 01-loops.js
+++ 01-loops.js
@@ -3,22 +3,27 @@ let students = [{name:'Albert'},{name:'Bob'},{name:'Claudia'}]
 let mix = [5, 'David', {name:'Ella'}, 23, 'Frank']
 
 // 20101
-// numbers.forEach(n =>{
-// 	console.log(n)
-// })
+// let logNumbers = (num) =>{
+// 	num.forEach(n =>{
+// 		console.log(n)
+// 	})
+// }
+//
+// logNumbers(numbers)
 
 //20102
-let logStudents = (st) => {
-	st.forEach(s => {
-		console.log(s.name)
-	})
-}
-
-logStudents(students)
-
-
-
+// let logStudents = (st) => {
+// 	st.forEach(s => {
+// 		console.log(s.name)
+// 	})
+// }
+//
+// logStudents(students)
 
 //20103'
-// let array = [2, 91, 79, 4, 23, 188, 22]
-// let newArray = array.map
+
+let add1ToArrayElements = (a) =>{
+	return a.map(n => n + 1)
+}
+
+console.log(add1ToArrayElements(numbers))
