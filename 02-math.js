da4f6ef3adbe590f8e30bb639055bb399c580b49
Ly <lydialawli@gmail.com>
2019-08-16 10:26:31 +0700

    added task 20202

--- 02-math.js
+++ 02-math.js
@@ -4,8 +4,20 @@ let products = [
 	{name:'gum',price:2.8}]
 
 //20201
-let round = (n) => {
+let roundNum = (n) => {
 	return Math.round(n)
 }
 
-console.log(round(15.9))
+//console.log(round(15.9))
+
+//20202
+let roundPrices = (p) => {
+	return p.map( i => {
+		return {
+			name: i.name,
+			age: roundNum(i.price)
+		}
+	})
+}
+
+console.log(roundPrices(products))
