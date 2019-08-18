20b69bf62ac9d10aeae68fc7c916408a7415c642
Ly <lydialawli@gmail.com>
2019-08-18 16:28:04 +0700

    added time tasks

--- 04-time.js
+++ 04-time.js
@@ -1,14 +1,15 @@
 // //20401
-// let a = () => console.log('Hello')
-//
-// let b = (lala,t) => {
-// 	setTimeout(lala(), t*1000)
-// }
-//
-// console.log(b(a,5))
-//
-// //(a, 3) // should log 'Hello' after 3 seconds
-// //(a, 5) // should log 'Hello' after 5 seconds
+let a = () => console.log('Hello')
+
+let b = (logHello,t) => {
+	setTimeout(logHello, t*1000)
+}
+
+console.log(b(a,5))
+
+
+//(a, 3) // should log 'Hello' after 3 seconds
+//(a, 5) // should log 'Hello' after 5 seconds
 
 
 //20402
